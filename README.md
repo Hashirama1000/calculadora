b1ab94788d20f70d114ce65038056dc45cd61745
unknown <Hashirama1000>
2021-06-22 22:24:26 -0600

    subiendo primera tarea

---
+++ README.md
@@ -0,0 +1,11 @@
+# calculadora
+calculadora en xamarin
+
+el proyecto esta en este repositorio:
+https://github.com/koryroot/calculadora
+link del video de como se programo:
+https://drive.google.com/file/d/1Jt6gkGO-hzvaV2BgC-dUfW2jNVdueGzE/view?usp=sharing
+link del video de como funciona:
+https://drive.google.com/file/d/1e6IeMCVUDSvVKVQRlNzJPGmPd-Y6kzd5/view?usp=drivesdk
+link del apk:
+https://drive.google.com/file/d/1l-22CRnLItsrcIPZQhI2ucF5QCwQA-Ws/view?usp=sharing 
---
+++ apkCalculadora/apkCalculadora/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace apkCalculadora
+{
+    public partial class MainPage : ContentPage
+    {
+        double res = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+            wkOperacion.Items.Add("suma");
+            wkOperacion.Items.Add("resta");
+            wkOperacion.Items.Add("multiplicacion");
+            wkOperacion.Items.Add("division");
+            wkOperacion.SelectedIndexChanged += WkOperacion_SelectedIndexChanged;
+            calcular.Clicked += Calcular_Clicked;
+            
+        }
+
+        private void WkOperacion_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Calcular_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtn1.Text) && !string.IsNullOrEmpty(txtn2.Text))
+            {
+                String op = wkOperacion.SelectedItem.ToString();
+                int n1 = Int32.Parse(txtn1.Text);
+                int n2 = Int32.Parse(txtn2.Text);
+
+
+                if (op == "suma")
+                {
+                    res = n1 + n2;
+                }
+                else if (op == "resta")
+                {
+                    res = n1 - n2;
+                }
+                else if (op == "multiplicacion")
+                {
+                    res = n1 * n2;
+                }
+                else if (op == "division")
+                {
+                    res = n1 / n2;
+                }
+                else
+                {
+                    Console.WriteLine("no se pueden hacer calculos multiples");
+                }
+
+            }
+            else
+            {
+                DisplayAlert("Datos erroneos","Por favor llene los cuadros de texto correctamente", "Ok");
+            }
+            lblRes.Text = res + "";
+        }
+    }
+}
